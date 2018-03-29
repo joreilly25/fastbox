703c13f0665e078bb418fce2bba82a39e4e08bfb
Justin O'Reilly <joreilly25@gmail.com>
2018-03-29 16:03:50 -0400

    Update fastbox.js

--- fastbox.js
+++ fastbox.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fast Box
 // @namespace    http://tampermonkey.net/
-// @version      3.0
+// @version      3.5
 // @description  Boxes... only FASTER!
 // @author       Justin O'Reilly
 // @match        https://crm.welcomewagon.com/index.php?module=Invoice&view=Edit*
