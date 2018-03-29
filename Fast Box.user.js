0b48224e5541ff6f3d7027b44c1007a5da0a8af2
Justin O'Reilly <joreilly25@gmail.com>
2018-03-29 15:33:47 -0400

    download file

---
+++ Fast Box.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Fast Box
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Boxes... only FASTER!
+// @author       Justin O'Reilly
+// @match        https://crm.welcomewagon.com/index.php?module=Invoice&view=Edit*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+     $("#lineItemTab :checkbox").click(function() {
+         var startitem = $(this).prop('id');
+         var numselect = parseInt(prompt("How many would you like to check?"));
+         var counter = 0;
+         var startnum = startitem.replace(/\D/g,'');
+         var orignum = startitem.replace(/\D/g,'');
+         if (numselect < 0) numselect = 0;
+         if (numselect > 1000) numselect = 1000;
+      if (isNaN(numselect)) {
+        $("#lineFiles :checkbox").prop("checked", !$("#lineFiles :checkbox").prop("checked"));
+      }
+      else {
+          $("#lineFiles" + orignum).prop('checked', false);
+          $("#lineItemTab :checkbox").prop('checked', false);
+        while (counter <= numselect) {
+          $("#lineFiles" + startnum).prop('checked', true);
+              startnum++;
+              counter++;
+              //console.log($("#lineFiles" + startnum));
+              //console.log(startnum);
+          }
+          $("#lineFiles" + orignum).prop('checked', false);
+      }
+});
+})();
